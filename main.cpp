afce8f6b7f8ca3921f290c91cfb94b74de26fc86
onitn <sandortares@gmail.com>
2024-01-12 22:08:55 +0100

    First

--- main.cpp
+++ main.cpp
@@ -3,25 +3,24 @@
 void startNcurses();
 
 int main() {
-    startNcurses();
-    srand(time(nullptr));
-    Menu menu = Menu();
-    menu.menuLoop();
+    startNcurses(); //Start Ncurses
+    srand(time(nullptr)); //Random seed
+    Menu menu = Menu(); //New menu instance
+    menu.menuLoop(); //Start menu
     return 0;
 }
 
 
-void startNcurses()
+void startNcurses() //Ncurses initialization
 {
-    initscr();
-    refresh();
-    noecho();
-    curs_set(0);
+    initscr(); //Screen initialization
+    refresh(); //Refresh (Update) Screen
+    noecho(); //No text shown when input is pressed
+    curs_set(0); //No text cursor (blinking line)
     keypad(stdscr, true);
-    timeout(FRAMERATE);
-    if (has_colors()) start_color();
-    init_pair(1,COLOR_RED, COLOR_BLACK);
-    if (can_change_color()) init_color(COLOR_WHITE, 1000, 498, 152);
+    timeout(FRAMERATE); //Max wait time between inputs
+    if (has_colors()) start_color(); //Start color mode if supported
+    init_pair(1,COLOR_RED, COLOR_BLACK); //Start each color
     init_pair(2,COLOR_WHITE, COLOR_BLACK);
     init_pair(3,COLOR_YELLOW, COLOR_BLACK);
     init_pair(4,COLOR_GREEN, COLOR_BLACK);
